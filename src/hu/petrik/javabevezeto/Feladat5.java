799f435fbecd6d55818e4962145d66f8c7820b8d
zoli <suvegeszoltan28@gmail.com>
2022-09-08 10:26:01 +0200

    feladat 5

--- src/hu/petrik/javabevezeto/Feladat5.java
+++ src/hu/petrik/javabevezeto/Feladat5.java
@@ -1,4 +1,31 @@
 package hu.petrik.javabevezeto;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Feladat5 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kérem adjameg a téglalap 'a' oldalát: ");
+        double a = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Kérem adjameg a téglalap 'b' oldalát: ");
+        double b = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Kérem adja meg hogy mit szeretne kiszámítani (K:kerüéet, T:terület): ");
+        String operation = sc.nextLine();
+        if (operation.equals("K")){
+            double perimeter = 2*(a+b);
+            System.out.printf("A téglalap kerülete %.2f cm \n", perimeter);
+        }else if (operation.equals("T")){
+            double area = a*b;
+            System.out.printf("A téglalap területe %.2f cm^2 \n", area);
+        }else{
+            System.out.println("Hibás műveletett adott meg!");
+        }
+
+        if (a == b){
+            System.out.println("A megadott terület egy négyzet!");
+        }
+    }
 }
