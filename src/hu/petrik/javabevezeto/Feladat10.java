a8f0468253d30120368cccd0f719db5101179155
zoli <suvegeszoltan28@gmail.com>
2022-09-08 10:45:24 +0200

    feladat 10

---
+++ src/hu/petrik/javabevezeto/Feladat10.java
@@ -0,0 +1,23 @@
+package hu.petrik.javabevezeto;
+
+import java.util.Scanner;
+
+public class Feladat10 {
+    public static void main(String[] args) {
+        int amount = 0;
+        int even = 0;
+        int odd = 0;
+        Scanner sc = new Scanner(System.in);
+        while (amount <= 100) {
+            System.out.print("Adjon meg egy számot: ");
+            int num = sc.nextInt();
+            amount += num;
+            if (num % 2 == 0) {
+                even++;
+            }else{
+                odd++;
+            }
+        }
+        System.out.printf("Páratlan számok: %d, Páros számok: %d", odd, even);
+    }
+}
