ddbc155c457ff13419b4876b5dede2d64add736f
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-09-12 14:12:56 +0200

    Feladat 13

---
+++ src/hu/petrik/javabevezeto/Feladat13.java
@@ -0,0 +1,24 @@
+package hu.petrik.javabevezeto;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Feladat13 {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        int num = rnd.nextInt(1000) + 1;
+        System.out.println("Tippeljen a gondolt számra amíg nem találja el!");
+        Scanner sc = new Scanner(System.in);
+        int guess = sc.nextInt();
+        while (num != guess){
+            if (num > guess){
+                System.out.println("A gondolt szám nagyobb!");
+                guess = sc.nextInt();
+            } else if (num < guess) {
+                System.out.println("A gondolt szám kissebb!");
+                guess = sc.nextInt();
+            }
+        }
+        System.out.println("Sikeresen megtippelte a számot! " + num);
+    }
+}
