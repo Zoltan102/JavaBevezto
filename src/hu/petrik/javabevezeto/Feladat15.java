0e8482398781cc6a0aeafd44075160f8b68839d9
zoli <suvegeszoltan28@gmail.com>
2022-09-13 16:26:04 +0200

    Feladat15

--- src/hu/petrik/javabevezeto/Feladat15.java
+++ src/hu/petrik/javabevezeto/Feladat15.java
@@ -1,4 +1,13 @@
 package hu.petrik.javabevezeto;
 
 public class Feladat15 {
+    public static void main(String[] args) {
+        double[] array = new double[25];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = Math.random() * 200 - 100;
+        }
+        for (int i = 0; i < array.length; i += 2) {
+            System.out.print(Math.pow(array[i], 2) + "; ");
+        }
+    }
 }
