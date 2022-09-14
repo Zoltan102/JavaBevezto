1ffc78afdebc107f6b6b9d92eb2b6577f48ca685
Zolizoli110 <73071177+Zolizoli110@users.noreply.github.com>
2022-09-14 19:32:38 +0200

    Feladat 17

--- src/hu/petrik/javabevezeto/Feladat17.java
+++ src/hu/petrik/javabevezeto/Feladat17.java
@@ -1,4 +1,26 @@
 package hu.petrik.javabevezeto;
 
+import java.util.Random;
+
 public class Feladat17 {
+    public static void main(String[] args) {
+        int[] array0 = new int[10];
+        int[] array1 = new int[10];
+        int[] array2 = new int[10];
+        Random rnd = new Random();
+        for (int i = 0; i < array0.length; i++) {
+            array0[i] = rnd.nextInt(101);
+            System.out.print(array0[i] + "; ");
+        }
+        System.out.println();
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = rnd.nextInt(101);
+            System.out.print(array1[i] + "; ");
+        }
+        System.out.println();
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = array0[i] + array1[i];
+            System.out.print(array2[i] + "; ");
+        }
+    }
 }
